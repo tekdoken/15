3db63c7c982183fb0f1d86dcebfd8acc30928501
tekdoken <tekdokenzx@gmail.com>
2021-09-23 11:37:53 +0700

    15

---
+++ blt2/giaithua.js
@@ -0,0 +1,5 @@
+function giaiThua(a){
+    if(a == 2)
+        return 2;
+    return a * giaiThua(a - 1);
+}
---
+++ blt2/kiemtraso.js
@@ -0,0 +1,7 @@
+function kiemTraSo(a){
+    if (a%1==0){
+      alert(true);
+    }else {
+        alert(false);
+    }
+}
